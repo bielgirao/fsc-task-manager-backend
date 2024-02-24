5d48a8d94355908bbf7a5ee757fcc7331d779dac
bielgirao <bielgirao@hotmail.com>
2024-02-23 22:31:08 -0300

    feat: add route to delete task

--- index.js
+++ index.js
@@ -24,7 +24,27 @@ app.post('/tasks', async (req, res) => {
     try {
         const newTask = new TaskModel(req.body);
         await newTask.save();
-        res.status(201).send(newTask);
+        res.status(201).send({
+            message: 'Task created successfully.',
+            newTask,
+        });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const taskToDelete = await TaskModel.findById(taskId);
+        if (!taskToDelete) {
+            return res.status(500).send('Task not found.');
+        }
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+        res.status(200).send({
+            message: 'Task deleted successfully.',
+            deletedTask,
+        });
     } catch (error) {
         res.status(500).send(error.message);
     }
