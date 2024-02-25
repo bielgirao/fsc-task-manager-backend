b94af415f9d01e9691ae9d62eaa59a0f3324683f
bielgirao <bielgirao@hotmail.com>
2024-02-25 16:24:20 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const connectToDatabase = require('./src/database/mongoose.database');
 const TaskRouter = require('./src/routes/task.routes');
@@ -7,6 +8,7 @@ const TaskRouter = require('./src/routes/task.routes');
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.APP_PORT || 8000;
 app.use(express.json());
 
